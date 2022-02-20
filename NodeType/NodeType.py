d8d062f8a8849b322d812bb8ef84ecf5651f8123
richard-creations <rykontechsolutions@gmail.com>
2022-02-20 15:07:13 -0600

    first commit

--- NodeType/NodeType.py
+++ NodeType/NodeType.py
@@ -67,8 +67,6 @@ def isSSN(data: any) -> bool:
     if re.match(r"^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$", sys.argv[1]):
         return True
 
-
-
 def isSex(data: any) -> bool:
     #check if it is a valid sex
     sexes = ["female", "male"]
