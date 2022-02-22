f2852c71c050d82cbe67c47ed2adaee126812c60
richard-creations <rykontechsolutions@gmail.com>
2022-02-22 04:19:06 -0600

    de-identification framework dev

--- de-identification/deidentify.py
+++ de-identification/deidentify.py
@@ -1,1 +1,24 @@
+import NodeType
+import yaml
+import pprint
 
+
+def deidentify(data=None, policy=None):
+    pass
+
+
+def parse_policy(policy):
+    primary_cmds = ("show", "blur", "hide", "none")
+    positions = ("last", "first", "index")
+    range_cmds = ("to")
+    policy_dict = {}
+    with open(policy, "r") as stream:
+        try:
+            policy_dict = yaml.safe_load(stream)
+            print(policy_dict)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+policy = "/Users/richardm/Documents/app_dev/APP-BASE/PrivacyLab/de-identification/policies/sample_policy.yml"
+parse_policy(policy)
